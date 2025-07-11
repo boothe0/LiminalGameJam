5bda9f9ba07ffd520879ded5af160f539dd6d802
Elizabeth <elizabethbooth05@gmail.com>
2025-07-10 22:02:58 -0700

    added some quest itemsto work with script

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -3,4 +3,4 @@ extends CanvasLayer
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/non_quest/main.tscn")
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	audio_stream_player.stop()
-	get_tree().change_scene_to_file("res://scenes/intro.tscn")
+	get_tree().change_scene_to_file("res://scenes/non_quest/intro.tscn")
 
 func _on_head_bob_pressed() -> void:
 	if Globals.motion_sickness_flag == true:
