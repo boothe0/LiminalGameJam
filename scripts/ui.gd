728f530c33f5ca4e7441ab0dd532c1f156ac38d0
Ranarh <jsl@jsl-art.de>
2025-07-21 23:21:05 +0200

    beautification and menu stuff
    tried adding a credits screen but buttons aren't working just yet

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -24,3 +24,7 @@ func _on_head_bob_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/non_quest/credits.tscn")
